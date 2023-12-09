073d8c67c55942f25602b2c960485fd6aa9e061a
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2023-12-08 16:06:36 -0800

    Day 6 part 2 solution

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,7 +2,7 @@ advent_of_code::solution!(6);
 
 /*
 Part 1 solution:
-1. strip labels for each row and turn numerals into a vector of u32
+1. strip labels for each row and turn numerals into a vector of u64
 2. for each pair of time and distance numbers, compute the number of winning possible combinations (see compute_winning_combos body)
 */
 
@@ -13,16 +13,16 @@ fn strip_line_prefix(input: Option<&str>, character: char) -> &str {
     }
 }
 
-fn string_to_digits(input: &str) -> Vec<u32> {
+fn string_to_digits(input: &str) -> Vec<u64> {
     input
         .split_whitespace()
-        .filter_map(|s| s.parse::<u32>().ok())
+        .filter_map(|s| s.parse::<u64>().ok())
         .collect()
 }
 
-fn compute_winning_combos(time: &u32, distance: &u32) -> u32 {
-    let mut winning_combo_count: u32 = 0;
-    let mut current_ms: u32 = 1;
+fn compute_winning_combos(time: &u64, distance: &u64) -> u64 {
+    let mut winning_combo_count: u64 = 0;
+    let mut current_ms: u64 = 1;
     let mut reached_record_range: bool = false;
 
     while current_ms < *time {
@@ -45,15 +45,15 @@ fn compute_winning_combos(time: &u32, distance: &u32) -> u32 {
     return winning_combo_count;
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut result: u32 = 1;
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut result: u64 = 1;
     let mut lines = input.lines();
     let time_line: Option<&str> = lines.next();
     let distance_line = lines.next();
     let time_string: &str = strip_line_prefix(time_line, ':');
     let distance_string: &str = strip_line_prefix(distance_line, ':');
-    let time_values: Vec<u32> = string_to_digits(time_string);
-    let distance_values: Vec<u32> = string_to_digits(distance_string);
+    let time_values: Vec<u64> = string_to_digits(time_string);
+    let distance_values: Vec<u64> = string_to_digits(distance_string);
 
     for (time, distance) in time_values.iter().zip(distance_values.iter()) {
         result *= compute_winning_combos(time, distance);
@@ -61,8 +61,25 @@ pub fn part_one(input: &str) -> Option<u32> {
     return Some(result);
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn string_to_single_digit(input: &str) -> Result<u64, std::num::ParseIntError> {
+    let combined = input.split_whitespace().collect::<String>();
+    combined.parse::<u64>()
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut lines = input.lines();
+    let time_line: Option<&str> = lines.next();
+    let distance_line = lines.next();
+    let time_string: &str = strip_line_prefix(time_line, ':');
+    let distance_string: &str = strip_line_prefix(distance_line, ':');
+    let time_value: Result<u64, std::num::ParseIntError> = string_to_single_digit(time_string);
+    let distance_value: Result<u64, std::num::ParseIntError> =
+        string_to_single_digit(distance_string);
+    let result = compute_winning_combos(&time_value.unwrap(), &distance_value.unwrap());
+    // for (time, distance) in time_values.iter().zip(distance_values.iter()) {
+    //     result *= compute_winning_combos(time, distance);
+    // }
+    return Some(result);
 }
 
 #[cfg(test)]
